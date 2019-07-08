0561c6c75e38649f0995f4d45ed97d321b98649b
Sachinm78 <sachinm78@gmail.com>
2019-07-08 13:52:45 -0700

    set up shows controller for html and json responses

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,8 +3,20 @@ class ShowsController < ApplicationController
 
     def index
         @shows = Show.all
+        respond_to do |format|
+            format.html {render :index}
+            format.json {render :json => @shows}
+        end
     end
 
+    def show
+        find_show
+        respond_to do |format|
+            format.html {render :show}
+            format.json {render :json => @show}
+        end
+    end
+    
     def new
         @show = Show.new
         @user = current_user
@@ -14,16 +26,13 @@ class ShowsController < ApplicationController
         @show = Show.new(show_params)
         if @show.save
             @show.reviews.create(show_id: @show.id)
-            redirect_to shows_path(@show)
+            respond_to do |format|
+                format.html {redirect_to @show}
+                format.json {render :json => @show}
+            end
         else
             render :new
         end
-    end
-
-    def show
-        find_show
-        @user = current_user
-        @reviews = show.reviews
     end
 
     def edit
@@ -37,7 +46,10 @@ class ShowsController < ApplicationController
         # find_user
         # @user = (@user == current_user ? @user : current_user)
         if @show.save
-            redirect_to show_path(@show)
+            respond_to do |format|
+                format.html {redirect_to @show}
+                format.json {render :json => @show}
+            end
         else
             render :edit
         end
@@ -46,8 +58,10 @@ class ShowsController < ApplicationController
     def destroy
         @user = current_user
         find_show.destroy
-        
-        redirect_to shows_path(@show) 
+        respond_to do |format|
+            format.html {redirect_to @shows}
+            format.json {head :no_content}
+        end
     end
 
     def find_user
