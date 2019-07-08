54ee96c734d07deaecde83d3e2232d78cb151093
Sachinm78 <sachinm78@gmail.com>
2019-07-08 15:20:00 -0700

    testing js on shows index page; encountering errors

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -9,5 +9,26 @@ function getShow(){
         dataType: 'json',
     }).done(function (data){
         console.log(data)
+        let firstShow = new Show(data[0])
+        let myShowHTML = firstShow.showHTML()
+        document.getElementById('new-posts').innerHTML += myShowHTML
     })
+}
+
+class Show {
+    cunstructor (obj){
+        this.id = obj.id
+        this.user_id = obj.user_id
+        this.title = obj.title
+        this.genre = obj.genre
+    }
+}
+
+Show.prototype.showHTML = function(){
+    return(`
+        <div>
+            <h3>${this.title}</h3>
+            <p>${this.genre}</p>
+        </div>
+    `)
 }
